a16a3ae0e89f97d5be9c4f1c994135f736ea93f5
wapl <33437769+wapl@users.noreply.github.com>
2019-11-20 10:52:03 +0800

    Create POst

---
+++ app/src/main/java/com/example/androidchallengeblog/CreateBlog.kt
@@ -0,0 +1,12 @@
+package com.example.androidchallengeblog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateBlog : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_blog)
+    }
+}
